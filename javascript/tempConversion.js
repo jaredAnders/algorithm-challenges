7b86eec52a9cd1b8f2f00534fe8ab9d7f620b3fa
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-21 10:27:20 -0500

    refactors switch statement, curbs output enthusiasm

--- javascript/tempConversion.js
+++ javascript/tempConversion.js
@@ -9,16 +9,14 @@ function convert (selection) {
   var conversion = selection == 0 ? {from: 'F', to: 'C'}: {from: 'C', to: 'F'};
   var degrees = Number(readlineSync.question('Enter degrees '+ conversion.from + ': '));
   degrees = selection == 0 ? (degrees - 32) * 5 / 9 : degrees = (degrees * 9 / 5) + 32;
-  console.log('It\'s ' + degrees.toFixed(1) + ' degrees '+ conversion.to +' outside!');
+  console.log('It\'s ' + degrees.toFixed(1) + ' degrees '+ conversion.to +' outside');
 };
 
+//run conversion
 switch(selection) {
-  case 0:
-    convert(selection);
-    break;
-  case 1:
-    convert(selection);
-    break;
   case -1:
     console.log('Conversion cancelled');
+    break;
+  default:
+    convert(selection);
 };
