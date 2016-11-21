5c161e8974afdce79c559fead543155e2dbaaa38
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-21 10:22:23 -0500

    refactors conversion

--- javascript/tempConversion.js
+++ javascript/tempConversion.js
@@ -2,19 +2,23 @@ var readlineSync = require('readline-sync');
 
 //choose conversion type
 var conversionTypes = ['Fahrenheit to Celsius', 'Celsius to Fahrenheit'];
-var selection = readlineSync.keyInSelect(conversionTypes, 'Convert: ');
-console.log(conversionTypes[selection]);
+var selection = readlineSync.keyInSelect(conversionTypes, 'Choose conversion: ');
 
 //calculate conversion
-var degrees = Number(readlineSync.question('\nEnter degrees:'));
+function convert (selection) {
+  var conversion = selection == 0 ? {from: 'F', to: 'C'}: {from: 'C', to: 'F'};
+  var degrees = Number(readlineSync.question('Enter degrees '+ conversion.from + ': '));
+  degrees = selection == 0 ? (degrees - 32) * 5 / 9 : degrees = (degrees * 9 / 5) + 32;
+  console.log('It\'s ' + degrees.toFixed(1) + ' degrees '+ conversion.to +' outside!');
+};
 
-if ( selection == 0) {
-  degrees = (degrees - 32) * 5 / 9;
-} else if (selection == 1) {
-  degrees = (degrees * 9 / 5) + 32;
-} else {
-  console.log('Conversion cancelled');
-}
-
-//print results
-console.log('It\'s ' + degrees + ' degrees outside!');
+switch(selection) {
+  case 0:
+    convert(selection);
+    break;
+  case 1:
+    convert(selection);
+    break;
+  case -1:
+    console.log('Conversion cancelled');
+};
