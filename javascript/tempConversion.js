297c25649ffb03b9911f8b8675ea4301957162e7
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-21 09:44:51 -0500

    adds options to convert from f to c or c to f

--- javascript/tempConversion.js
+++ javascript/tempConversion.js
@@ -1,5 +1,20 @@
 var readlineSync = require('readline-sync');
 
-var degrees = Number(readlineSync.question('\nEnter degrees in Celsius: '));
-degrees = (degrees * 9 / 5) + 32;
-console.log('It is ' + degrees + ' degrees outside!');
+//choose conversion type
+var conversionTypes = ['Fahrenheit to Celsius', 'Celsius to Fahrenheit'];
+var selection = readlineSync.keyInSelect(conversionTypes, 'Convert: ');
+console.log(conversionTypes[selection]);
+
+//calculate conversion
+var degrees = Number(readlineSync.question('\nEnter degrees:'));
+
+if ( selection == 0) {
+  degrees = (degrees - 32) * 5 / 9;
+} else if (selection == 1) {
+  degrees = (degrees * 9 / 5) + 32;
+} else {
+  console.log('Conversion cancelled');
+}
+
+//print results
+console.log('It\'s ' + degrees + ' degrees outside!');
