61feff42c635b8485fb001f0d2ea1deb52e65f77
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-23 09:45:25 -0500

    splits number into array, doubles every other digit, subtracts product by 9 if over 10, sums all digits, checks if sum is divisible by 10 to validate

--- ruby/luhn/lib/luhn.rb
+++ ruby/luhn/lib/luhn.rb
@@ -1,5 +1,13 @@
 module Luhn
   def self.is_valid?(number)
-    #WRITE YOUR SOLUTION HERE
+    array = number.to_s.split(//)
+    sum = 0
+    array.reverse.each_with_index do |value, i|
+      value = value.to_i
+      value *= 2 if (i + 1) % 2 == 0
+      value -= 9 if value > 9
+      sum += value
+    end
+    sum % 10 == 0 ? true : false
   end
 end
