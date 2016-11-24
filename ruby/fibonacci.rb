e82faa18689c564cde1cdac33dd0fdad8d2fff2b
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-24 11:34:00 -0500

    adds recursive method

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,16 +1,22 @@
 require 'benchmark'
 
-def recursive_fib
+def recursive_fib(n)
+  if n == 0
+    return 0
+  elsif n == 1
+    return 1
+  else
+    return recursive_fib(n - 1) + recursive_fib(n - 2)
+  end
+end
+
+def iterative_fib(n)
 
 end
 
-def iterative_fib
-  
-end
-
-require 'benchmark'
-num = 35
-Benchmark.bm do |x|
-  x.report("recursive_fib") { recursive_fib(num) }
-  x.report("iterative_fib")  { iterative_fib(num)  }
-end
+num = 8
+print recursive_fib(num)
+# Benchmark.bm do |x|
+#   x.report("recursive_fib") { recursive_fib(num) }
+#   x.report("iterative_fib")  { iterative_fib(num)  }
+# end
