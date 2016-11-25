5a0ab67e98191505f185c99ca549e1287903942e
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-25 10:46:15 -0500

    adds iterative fibonacci method

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -11,12 +11,25 @@ def recursive_fib(n)
 end
 
 def iterative_fib(n)
-
+  sum = []
+  (n + 1).times do |x|
+    if x == 0
+      sum.push(0)
+    elsif x == 1
+      sum.push(1)
+    else
+      nextNum = sum[x-1] + sum[x-2]
+      sum.push(nextNum)
+    end
+  end
+  sum.last
 end
 
-num = 8
-print recursive_fib(num)
-# Benchmark.bm do |x|
-#   x.report("recursive_fib") { recursive_fib(num) }
-#   x.report("iterative_fib")  { iterative_fib(num)  }
-# end
+num = 35
+# print recursive_fib(num)
+# print "\n"
+# print iterative_fib(num)
+Benchmark.bm do |x|
+  x.report("recursive_fib") { recursive_fib(num) }
+  x.report("iterative_fib")  { iterative_fib(num)  }
+end
