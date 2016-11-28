ef4f94280c4ef9a53228c0e757aadab73ec45648
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-28 11:14:04 -0500

    adds method for depth first search

--- ruby/tree_traversal.rb
+++ ruby/tree_traversal.rb
@@ -6,6 +6,16 @@ class Tree
     @children = children
   end
 
+  def depth_first_search(search_value)
+    if @payload == search_value
+      puts self.inspect
+    else
+      @children.each do |child|
+        child.depth_first_search(search_value)
+      end
+    end
+  end
+
 end
 
 deep_fifth_node = Tree.new(5, [])
@@ -19,3 +29,6 @@ seventh_node = Tree.new(7, [sixth_node])
 shallow_fifth_node = Tree.new(5, [ninth_node])
 
 root = Tree.new(2, [seventh_node, shallow_fifth_node])
+
+# find value of 6 in root tree
+root.depth_first_search(6)
