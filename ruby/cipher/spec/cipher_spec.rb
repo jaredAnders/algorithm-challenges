61627dfd66b1433b191d95b1ef498b27f003634c
Jared Gilbert <jaredandersgilbert@gmail.com>
2017-01-20 11:43:57 -0500

    sets up project structure including tests for cipher challenge

---
+++ ruby/cipher/spec/cipher_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+RSpec.describe Cipher, type: :model do
+  describe '#decrypt' do
+    it 'returns correct message' do
+      test = true
+      expect(test).to eq true
+    end
+  end
+end
