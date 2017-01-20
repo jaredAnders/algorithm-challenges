957ba341b2b161018046dd813614fe16eba9f1ef
Jared Gilbert <jaredandersgilbert@gmail.com>
2017-01-20 13:21:30 -0500

    adds method to modify keyword for columnar transposition cipher

--- ruby/cipher/lib/cipher.rb
+++ ruby/cipher/lib/cipher.rb
@@ -1,3 +1,22 @@
 class Cipher
-  
+  attr_accessor :key, :decrypt
+
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+  end
+
+  def decrypt
+    order_keyword
+  end
+
+  def order_keyword
+    new_array = []
+    array = key.split('')
+    array.each_with_index do |e, i|
+      new_array << e unless array.index(e) != i
+    end
+    new_array.sort.join
+  end
 end
--- ruby/cipher/spec/cipher_spec.rb
+++ ruby/cipher/spec/cipher_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 
 RSpec.describe Cipher, type: :model do
   describe '#decrypt' do
-    it 'returns correct message' do
-      test = true
-      expect(test).to eq true
+    it 'returns key' do
+      message = "1T7766T56C6ZXB71T7NIXC6I5XMXIY11CWX8I5V7AE"
+      key = "CEILING"
+      date = 17
+      test = Cipher.new(message, key, date)
+      expect(test.order_keyword).to eq "CEGILN"
     end
   end
 end
