d0fceb7c7e8abe09df4220d5fcb930cda5f2dff8
Jared Gilbert <jaredandersgilbert@gmail.com>
2017-01-22 20:22:57 -0500

    adds working method to reverse columnar transposition cipher

--- ruby/cipher/lib/cipher.rb
+++ ruby/cipher/lib/cipher.rb
@@ -1,22 +1,56 @@
 class Cipher
-  attr_accessor :key, :decrypt
+  attr_accessor :text, :key, :date, :reverse_transpose
 
-  def initialize(message, key, date)
-    @message = message
+  def initialize(text, key, date)
+    @text = text
     @key = key
     @date = date
   end
 
-  def decrypt
-    order_keyword
+  # def decrypt
+  #   modify_keyword
+  # end
+
+  # def caesar
+  #
+  # end
+  #
+
+  def reverse_transpose
+    dedupe_key = dedupe.join
+    mod_key = modify_keyword
+    cut_length = text.length / mod_key.length
+    cuts = text.scan(/.{#{cut_length}}/)
+
+    hash = {}
+    dedupe_key.split('').each_with_index do |key_char, i|
+      hash[key_char.to_sym] = cuts[i]
+    end
+
+    cuts = []
+    mod_key.split('').each do |key_char|
+      cuts << hash[key_char.to_sym].split('')
+    end
+
+    reversed_text = cuts.transpose.join('')
+
+    # binding.pry
+
+    reversed_text.chop! while reversed_text.end_with? 'X'
+
+    reversed_text
   end
 
-  def order_keyword
+  def modify_keyword
+    dedupe.sort.join
+  end
+
+  def dedupe
     new_array = []
     array = key.split('')
     array.each_with_index do |e, i|
       new_array << e unless array.index(e) != i
     end
-    new_array.sort.join
+    new_array
   end
 end
