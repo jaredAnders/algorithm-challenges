ad2af4b8b793456aba7a2c5db49af4c796f0cdef
Jared Gilbert <jaredandersgilbert@gmail.com>
2017-01-23 10:26:50 -0500

    adds spec for full decryption

--- ruby/cipher/spec/cipher_spec.rb
+++ ruby/cipher/spec/cipher_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe Cipher, type: :model do
-  let(:date) { 17 }
+  describe '#decrypt' do
+    let(:text) { '1T7766T56C6ZXB71T7NIXC6I5XMXIY11CWX8I5V7AE' }
+    let(:keyword) { 'CEILING' }
+    let(:date) { 17 }
+    let(:input) { Cipher.new(text, keyword, date) }
+    it 'decrypts message' do
+      expected = 'IMPORTANTINFORMATIONCOMINGOVERNEXT4DAYS'
+      expect(input.decrypt).to eq expected
+    end
+  end
+
   describe '#reverse_caesar' do
     let(:text) { 'Q44N7YGSQRW' }
     let(:keyword) { 'GREYHOUND' }
+    let(:date) { 17 }
     let(:input) { Cipher.new(text, keyword, date) }
     it 'reverses caesar cipher' do
       expected = 'ALLYOURBASE'
@@ -15,6 +26,7 @@ RSpec.describe Cipher, type: :model do
   describe '#reverse_transposition' do
     let(:text) { 'ASTOTSVALNINDCXNMOUIHIROXYOWROMPGYA' }
     let(:keyword) { 'FOXHOUND' }
+    let(:date) { 17 }
     let(:input) { Cipher.new(text, keyword, date) }
     it 'reverses columnal transposition' do
       expected = 'MANYSHIPSMOVINGTOWARDYOURLOCATION'
