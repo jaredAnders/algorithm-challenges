df912008ee7ef460d37abcd79f16810b5dcc8dc4
Jared Gilbert <jaredandersgilbert@gmail.com>
2017-01-23 10:13:58 -0500

    removes modify_keyword test

--- ruby/cipher/spec/cipher_spec.rb
+++ ruby/cipher/spec/cipher_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 RSpec.describe Cipher, type: :model do
   let(:date) { 17 }
   describe '#reverse_caesar' do
-    let(:text) { 'ALL YOUR BASE' }
+    let(:text) { 'Q44N7YGSQRW' }
     let(:keyword) { 'GREYHOUND' }
     let(:input) { Cipher.new(text, keyword, date) }
     it 'reverses caesar cipher' do
-      expected = 'QSTVWXZ0123456789GREYHOUNDABCFIJKLMP'
+      expected = 'ALLYOURBASE'
       expect(input.reverse_caesar).to eq expected
     end
   end
@@ -19,15 +19,6 @@ RSpec.describe Cipher, type: :model do
     it 'reverses columnal transposition' do
       expected = 'MANYSHIPSMOVINGTOWARDYOURLOCATION'
       expect(input.reverse_transposition).to eq expected
-    end
-  end
-
-  describe '#modify_keyword' do
-    let(:text) { '1T7766T56C6ZXB71T7NIXC6I5XMXIY11CWX8I5V7AE' }
-    let(:keyword) { 'CEILING' }
-    let(:input) { Cipher.new(text, keyword, date) }
-    it 'dedupes and reorders characters alphabetically' do
-      expect(input.modify_keyword).to eq 'CEGILN'
     end
   end
 end
